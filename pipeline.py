af8861567ab67e657bc1686c7e83519e9f6ecf88
anirudhasundaresan <anirudhasundaresan@outlook.com>
2019-02-18 22:24:50 -0500

    adding testing phase

--- pipeline.py
+++ pipeline.py
@@ -1,18 +1,23 @@
+import warnings
+warnings.filterwarnings("ignore")
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import ElasticNetCV
 from sklearn.cross_validation import cross_val_score
 from sklearn.linear_model import LinearRegression
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
 
 train_csv = pd.read_csv("train_.csv")
 train_csv_dims = train_csv.shape
 print("Shape of the train csv file is: ", train_csv_dims) # 8000 x 101
-# x0        x1        x2        x3        x4        x5        x6            x7        x8    ...          x92       x93       x94       x95       x96       x97       x98       x99         y
 
 # Check which cols are numeric/ categorical
 cols = train_csv.columns # all cols
@@ -23,7 +28,7 @@ print("Categorical columns are: ", list(set(cols) - set(num_cols)))
 
 # we need count of unique values for all cols just to make sure any of the numeric values are not categorical
 for col in train_csv:
-    if len(train_csv[col].unique()) < 7500:
+    if len(train_csv[col].unique()) < 8000:
         print(col, len(train_csv[col].unique()))
         # verifies that x13, x68 and x91 are indeed the only categorical values
 # some cols have <=8000 unique values, hence this means some of them has nans
@@ -53,8 +58,6 @@ del train_csv['y']
 # let us scale data in all columns; regression anyway requires scaled data; RF does not require scaling. PCA must be done only after scaling
 # train_csv has only numerical values now; remember to use cat_train_csv while building the model
 
-scaler = StandardScaler()
-train_csv[train_csv.columns] = scaler.fit_transform(train_csv[train_csv.columns])
 # X_test = scaler.transform(X_test)
 # y = train_csv[train_csv.apply(lambda x :(x-x.mean()).abs()<(3*x.std()) ).all(1)] # we can do this to check for outliers since this is now scaled data
 # len(y) = 6712x98 which means that there are some outlier data - not sure if we should remove outliers since they might be important
@@ -88,7 +91,9 @@ print(corr_stats)
 sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns)
 # plt.show() # a bit dense, not much information can be gained; there are some highly correlated variables
 
-
+# use pipeline for this
+scaler = StandardScaler()
+train_csv[train_csv.columns] = scaler.fit_transform(train_csv[train_csv.columns])
 # it would help to model our task by using PCA, and to decide the number of components for PCA, we need the PCA curve with the explained variance
 pca = PCA(n_components=0.95, svd_solver='full') # make sure we have 95% variance explained when we transform
 train_csv_transformed = pca.fit_transform(train_csv)
@@ -100,19 +105,56 @@ train_csv_final = pd.concat([pd.DataFrame(train_csv_transformed), X], axis=1)
 # let us try regression; do cross validation
 regr = ElasticNetCV(cv=10, random_state=0)
 regr.fit(train_csv_final, label_train_csv)
-print(regr.score(train_csv_final, label_train_csv))
+print("R^2 score: ", regr.score(train_csv_final, label_train_csv))
 
-'''
+
 lm = LinearRegression()
-mse_scores = -cross_val_score(lm, train_csv_transformed, label_train_csv, cv=10, scoring='neg_mean_squared_error')
+mse_scores = -cross_val_score(lm, train_csv_final, label_train_csv, cv=10, scoring='neg_mean_squared_error')
 # fix the sign of MSE scores
 rmse_scores = np.sqrt(mse_scores)
 print(rmse_scores)
 # calculate the average RMSE
-print(rmse_scores.mean())
-# do elastic since we want feature selection and regularization
+print("Mean CV score: ", rmse_scores.mean())
+
+
+ridge = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(train_csv_final, label_train_csv)
+ridge.score(train_csv_final, label_train_csv)
+
+
+
+
+
+
+
+
+### Testing phase
+test_csv = pd.read_csv("test_.csv")
 '''
+# none of the categoricals are missing, I guess it is safe to impute with mean for the others
+>>> test_csv = pd.read_csv("test_.csv")
+>>> test_csv.count()['x13']
+2000
+>>> test_csv.count()['x68']
+2000
+>>> test_csv.count()['x91']
+2000
+'''
+test_csv.fillna(test_csv.mean(), inplace=True) # imputing for test data only
 
+test_csv_dims = test_csv.shape
+print("Shape of the test csv file is: ", test_csv_dims) # 8000 x 101
+# label_test_csv
+cat_test_csv = pd.concat([test_csv['x13'], test_csv['x68'], test_csv['x91']], axis=1)
+cat_test_csv.reset_index(inplace=True, drop=True)
+del test_csv['x13']
+del test_csv['x68']
+del test_csv['x91']
 
-# print(x)
-# print(y)
+test_csv[test_csv.columns] = scaler.transform(test_csv[test_csv.columns])
+# it would help to model our task by using PCA, and to decide the number of components for PCA, we need the PCA curve with the explained variance
+# pca = PCA(n_components=0.95, svd_solver='full') # make sure we have 95% variance explained when we transform
+test_csv_transformed = pca.transform(test_csv)
+X_ = pd.get_dummies(data=cat_test_csv, drop_first=True)
+test_csv_final = pd.concat([pd.DataFrame(test_csv_transformed), X_], axis=1)
+elastic_predict = regr.predict(test_csv_final)
+ridge_predict = ridge.predict(test_csv_final)
